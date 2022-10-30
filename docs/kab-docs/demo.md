69ed2e341e19d1bb6bfdea717288454d615e2468
kabagouda <technodor7@gmail.com>
2022-10-30 12:52:21 +0000

    update

--- docs/kab-docs/demo.md
+++ docs/kab-docs/demo.md
@@ -4,6 +4,7 @@ sidebar_position: 1
 # Kirikou
 
 :::danger Ceci c' est moi
+Ceci est un test
 :::
 
 ```dart
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,17 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "Kab Learning Doc",
   tagline: "Mini Flutter Documentation",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
+  url: "https://github.com",
+  baseUrl: "/kabdoc",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "kabdoc", // Usually your GitHub org/user name.
+  projectName: "kabdoc", // Usually your repo name.
+  deploymentBranch: "gh-pages", // Branch that GitHub pages will deploy from.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
