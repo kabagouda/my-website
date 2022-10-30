d50811cd02faf5dc7f353c1727fef4390b2ccf1b
kabagouda <technodor7@gmail.com>
2022-10-30 13:22:31 +0000

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,13 +37,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/kabagouda/my-website",
+          editUrl: "https://github.com/kabagouda/my-website/tree/main",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/kabagouda/my-website",
+          editUrl: "https://github.com/kabagouda/my-website/tree/main",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
